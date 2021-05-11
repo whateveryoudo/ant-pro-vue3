a1a15e6b2da9d0f14dcb7b2560ce376c6d693f71
youkaixing <1795261900@qq.com>
2021-05-11 18:01:10 +0800

    项目初始化

---
+++ src/views/login/index.vue
@@ -0,0 +1,22 @@
+<!--
+ * @Author: ykx
+ * @Date: 2021-05-11 17:13:32
+ * @LastEditTime: 2021-05-11 17:13:40
+ * @LastEditors: your name
+ * @Description: 
+ * @FilePath: \my-vue-app\src\views\login\index.vue
+-->
+<template>
+  <div>我是登录页</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  created() {},
+  mounted() {},
+};
+</script>
+<style scoped></style>
---
+++ types/global.d.ts
@@ -0,0 +1,9 @@
+/*
+ * @Author: ykx
+ * @Date: 2021-05-11 15:57:00
+ * @LastEditTime: 2021-05-11 16:42:20
+ * @LastEditors: your name
+ * @Description: 
+ * @FilePath: \my-vue-app\types\global.d.ts
+ */
+declare type Recordable<T = any> = Record<string, T>
